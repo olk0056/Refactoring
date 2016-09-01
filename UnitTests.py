1aaba56ccd38fc5d9d73da39c518d88c85b56914
olk0056 <oliviasking@gmail.com>
2016-09-01 13:31:16 +1200

    Test reworked to confirm string format works

--- UnitTests.py
+++ UnitTests.py
@@ -10,7 +10,6 @@ class Scraping_Test(unittest.TestCase):
         """testing output of soup scraping using original method"""
         r = requests.get("http://www.fishpond.co.nz/Books/Fiction_Literature")
         soup = BeautifulSoup(r.content, "lxml")
-        rrp_result = []
         rrp_results = []
         for rrp in soup.find_all('s'):
             try:
@@ -22,6 +21,16 @@ class Scraping_Test(unittest.TestCase):
 
     def test_name(self):
         '''
-        Testing code of string formatter for validity
+        Testing code of string formatter for validity, hardcoded as url is usually set through CMD before running
         '''
-        self.assertTrue(len(Webscraping.product_name_results) != 0)
+        r = requests.get("http://www.fishpond.co.nz/Books/Fiction_Literature")
+        soup = BeautifulSoup(r.content, "lxml")
+        product_name_results = []
+        name = '("string1", {"string2": "string3"}).format(string1="a", string2="class", string3="blue_link fn url")'
+        for product_name in soup.find_all(name):
+            try:
+                product_name_results.append(product_name.text)
+            except IndexError:
+                print("No products found found")
+        return product_name_results
+        self.assertTrue(len(Webscraping.product_name(self)) != 0)
