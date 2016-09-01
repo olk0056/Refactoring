50785576dd29d7e4270ae785a3d14ecadb4162f8
olk0056 <oliviasking@gmail.com>
2016-09-02 11:26:28 +1200

    Initial separation of publishing_data method for bad smell- Long Method

--- Data_Method.py
+++ Data_Method.py
@@ -5,42 +5,69 @@ from matplotlib import pyplot as plt
 
 class Data_Methods(Webscraping):
 
-    def price_comparison(webScraping):
-            x = []
-            count = 0
-            x1 = []
-            count1 = 0
-            intlist = [float(x) for x in webScraping.RRP()]
-            salePriceInt = [float(x) for x in webScraping.sale_prices()]
-            while count < len(webScraping.RRP()):
-                count += 1
-                x.append(count)
-
-            while count1 < len(webScraping.sale_prices()):
-                count1 += 1
-                x1.append(count1)
-            plt.figure(num=None, figsize=(10, 10), dpi=80,
-                       facecolor='w', edgecolor='k')
-            y = [i for i in intlist]
-            y1 = [i for i in salePriceInt]
-            plt.xlim(0, 22)
-            plt.plot(x, y, label='RRP')
-            plt.plot(x1, y1, label='Sale Price', color='r')
-            plt.scatter(x, y)
-            plt.scatter(x1, y1,  color='r')
-            plt.title('Price Comparison Chart')
-            plt.ylabel('Price ($)')
-            plt.xlabel('Number of products')
-            for i, j in zip(y1, x1):
-                plt.annotate(str(i), xy=(j, i), textcoords='data',
-                             horizontalalignment='left',
-                             verticalalignment='bottom')
-            for i, j in zip(y, x):
-                plt.annotate(str(i), xy=(j, i), textcoords='data',
-                             horizontalalignment='left',
-                             verticalalignment='bottom')
-            plt.legend()
+    def products_table(webscraping):
+            fig, axs = plt.subplots(2, 1)
+            clust_data = (webscraping.product_name(),
+                          webscraping.isbn(),
+                          webscraping.publishing_data(),
+                          webscraping.RRP(),
+                          webscraping.sale_prices(),
+                          webscraping.saving_total(),
+                          webscraping.photo_link())
+            collabel = ("Product Name", "ISBN", "Publishing Date", "RRP (NZ$)",
+                        "Sale Price (NZ$)", "Savings (%)")
+            axs[0].axis('tight')
+            axs[0].axis('off')
+            the_table = axs[0].table(cellText=clust_data,
+                                     colLabels=collabel, loc='center')
             plt.show()
+
+    def scatter_graph(webScraping):
+        x = []
+        count = 0
+        intlist = [float(x) for x in webScraping.RRP()]
+        while count < len(webScraping.RRP()):
+            count += 1
+            x.append(count)
+        plt.figure(num=None, figsize=(10, 10), dpi=80,
+                   facecolor='w', edgecolor='k')
+        y = [i for i in intlist]
+        plt.xlim(0, 22)
+        plt.plot(x, y, label='RRP')
+        plt.scatter(x, y)
+        plt.title('Price Comparison Chart')
+        plt.ylabel('Price ($)')
+        plt.xlabel('Number of products')
+        for i, j in zip(y, x):
+            plt.annotate(str(i), xy=(j, i), textcoords='data',
+                         horizontalalignment='left',
+                         verticalalignment='bottom')
+
+    def plot_graph(webScraping):
+        x1 = []
+        count1 = 0
+        salePriceInt = [float(x) for x in webScraping.sale_prices()]
+        while count1 < len(webScraping.sale_prices()):
+            count1 += 1
+            x1.append(count1)
+        plt.figure(num=None, figsize=(10, 10), dpi=80,
+                   facecolor='w', edgecolor='k')
+        y1 = [i for i in salePriceInt]
+        plt.xlim(0, 22)
+        plt.plot(x1, y1, label='Sale Price', color='r')
+        plt.scatter(x1, y1, color='r')
+        for i, j in zip(y1, x1):
+            plt.annotate(str(i), xy=(j, i), textcoords='data',
+                         horizontalalignment='left',
+                         verticalalignment='bottom')
+
+
+
+    def plot_scatter_graph(webScraping):
+        Data_Methods.plot_graph(webScraping)
+        Data_Methods.scatter_graph(webScraping)
+        plt.legend()
+        plt.show()
 
     def total_savings_data(webScraping):
             count = 0
@@ -106,21 +133,21 @@ class Data_Methods(Webscraping):
                 elif strings == "December":
                             December.append(1)
 
-                # Data to plot
-            labels = ['January', 'February', 'March', 'April', 'May', 'June',
-                      'July', 'August', 'September', 'October',
-                      'November', 'December']
-            sizes = [len(January), len(February), len(March), len(April),
-                     len(May), len(June), len(July),
-                     len(August), len(September), len(October),
-                     len(November), len(December)]
-            colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue',
-                      'red', 'black', 'blue', 'yellow', 'green',
-                      'purple', 'orange', 'darkblue']
-            explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    def print_publisher(webScraping):
+        # Data to plot
+        p = Data_Methods.publishing_data();
+        labels = ['January', 'February', 'March', 'April', 'May', 'June',
+                  'July', 'August', 'September', 'October',
+                  'November', 'December']
+        sizes = [len(p.January), len(p.February), len(p.March), len(p.April), len(p.May), len(p.June), len(p.July),
+                 len(p.August), len(p.September), len(p.October), len(p.November), len(p.December)]
+        colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue',
+                  'red', 'black', 'blue', 'yellow', 'green',
+                  'purple', 'orange', 'darkblue']
+        explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
-            plt.pie(sizes, explode=explode, labels=labels, colors=colors,
-                    autopct='%1.0f%%', pctdistance=1.1, labeldistance=1.2)
+        plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+                autopct='%1.0f%%', pctdistance=1.1, labeldistance=1.2)
 
-            plt.axis('equal')
-            plt.show()
+        plt.axis('equal')
+        plt.show()
