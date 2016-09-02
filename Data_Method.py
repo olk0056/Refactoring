ea2277de5099611df14e708c738bafad66ee67e7
olk0056 <oliviasking@gmail.com>
2016-09-02 12:21:54 +1200

    Reworked method extraction for Bad Smell - Long Method

--- Data_Method.py
+++ Data_Method.py
@@ -2,25 +2,69 @@ from Scraping import *
 import re
 from matplotlib import pyplot as plt
 
-
 class Data_Methods(Webscraping):
 
-    def products_table(webscraping):
-            fig, axs = plt.subplots(2, 1)
-            clust_data = (webscraping.product_name(),
-                          webscraping.isbn(),
-                          webscraping.publishing_data(),
-                          webscraping.RRP(),
-                          webscraping.sale_prices(),
-                          webscraping.saving_total(),
-                          webscraping.photo_link())
-            collabel = ("Product Name", "ISBN", "Publishing Date", "RRP (NZ$)",
-                        "Sale Price (NZ$)", "Savings (%)")
-            axs[0].axis('tight')
-            axs[0].axis('off')
-            the_table = axs[0].table(cellText=clust_data,
-                                     colLabels=collabel, loc='center')
-            plt.show()
+    def setter(self):
+       global January, February, March, April, May, June, July, August, September, October, November, December
+       January = []
+       February = []
+       March = []
+       April = []
+       May = []
+       June = []
+       July = []
+       August = []
+       September = []
+       October = []
+       November = []
+       December = []
+
+       result = re.findall(r"(?i)\b[a-z]+\b", str
+                 (Webscraping.publishing_date(self)))
+
+       for strings in result:
+            if strings == "January":
+                January.append(1)
+            elif strings == "February":
+                February.append(1)
+            elif strings == "March":
+                March.append(1)
+            elif strings == "April":
+                April.append(1)
+            elif strings == "May":
+                May.append(1)
+            elif strings == "June":
+                June.append(1)
+            elif strings == "July":
+                July.append(1)
+            elif strings == "August":
+                August.append(1)
+            elif strings == "September":
+                September.append(1)
+            elif strings == "October":
+                October.append(1)
+            elif strings == "November":
+                November.append(1)
+            elif strings == "December":
+                December.append(1)
+
+    def print_publisher(webScraping, setter):
+        # Data to plot
+        labels = ['January', 'February', 'March', 'April', 'May', 'June',
+                  'July', 'August', 'September', 'October',
+                  'November', 'December']
+        sizes = [len(January), len(February), len(March), len(April), len(May), len(June), len(July),
+                 len(August), len(September), len(October), len(November), len(December)]
+        colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue',
+                  'red', 'black', 'blue', 'yellow', 'green',
+                  'purple', 'orange', 'darkblue']
+        explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+        plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+                autopct='%1.0f%%', pctdistance=1.1, labeldistance=1.2)
+
+        plt.axis('equal')
+        plt.show()
 
     def scatter_graph(webScraping):
         x = []
@@ -90,64 +134,3 @@ class Data_Methods(Webscraping):
                              horizontalalignment='left',
                              verticalalignment='bottom')
             plt.show()
-
-    def publishing_data(webScraping):
-            January = []
-            February = []
-            March = []
-            April = []
-            May = []
-            June = []
-            July = []
-            August = []
-            September = []
-            October = []
-            November = []
-            December = []
-
-            result = re.findall(r"(?i)\b[a-z]+\b", str
-                                (webScraping.publishing_date()))
-            for strings in result:
-                if strings == "January":
-                            January.append(1)
-                elif strings == "February":
-                            February.append(1)
-                elif strings == "March":
-                            March.append(1)
-                elif strings == "April":
-                            April.append(1)
-                elif strings == "May":
-                            May.append(1)
-                elif strings == "June":
-                            June.append(1)
-                elif strings == "July":
-                            July.append(1)
-                elif strings == "August":
-                            August.append(1)
-                elif strings == "September":
-                            September.append(1)
-                elif strings == "October":
-                            October.append(1)
-                elif strings == "November":
-                            November.append(1)
-                elif strings == "December":
-                            December.append(1)
-
-    def print_publisher(webScraping):
-        # Data to plot
-        p = Data_Methods.publishing_data();
-        labels = ['January', 'February', 'March', 'April', 'May', 'June',
-                  'July', 'August', 'September', 'October',
-                  'November', 'December']
-        sizes = [len(p.January), len(p.February), len(p.March), len(p.April), len(p.May), len(p.June), len(p.July),
-                 len(p.August), len(p.September), len(p.October), len(p.November), len(p.December)]
-        colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue',
-                  'red', 'black', 'blue', 'yellow', 'green',
-                  'purple', 'orange', 'darkblue']
-        explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-
-        plt.pie(sizes, explode=explode, labels=labels, colors=colors,
-                autopct='%1.0f%%', pctdistance=1.1, labeldistance=1.2)
-
-        plt.axis('equal')
-        plt.show()
