e3b16e8d95b8429f7acda592aef10c6ea60cb785
olk0056 <oliviasking@gmail.com>
2016-09-14 14:35:03 +1200

    Tidied for pep8 compliance

--- Data_Method.py
+++ Data_Method.py
@@ -2,62 +2,35 @@ from Scraping import *
 import re
 from matplotlib import pyplot as plt
 
-class Data_Methods(object):
 
-    def setter(self):
-       global January, February, March, April, May, June, July, August, September, October, November, December
-       January = []
-       February = []
-       March = []
-       April = []
-       May = []
-       June = []
-       July = []
-       August = []
-       September = []
-       October = []
-       November = []
-       December = []
+class DataMethods(Webscraping):
+    def setter(self, webscraping):
+        months = ['January', 'February', 'March', 'April', 'May',
+                  'June', 'July', 'August', 'September', 'October',
+                  'November', 'December']
 
-       global result
-       result = re.findall(r"(?i)\b[a-z]+\b", str
-                 (Webscraping.publishing_date(self)))
+        global month_list
+        result = re.findall(r"(?i)\b[a-z]+\b", str
+                            (webscraping.publishing_date(self)))
 
-    def month_setter(self):
-        for strings in result:
-            if strings == "January":
-                January.append(1)
-            elif strings == "February":
-                February.append(1)
-            elif strings == "March":
-                March.append(1)
-            elif strings == "April":
-                April.append(1)
-            elif strings == "May":
-                May.append(1)
-            elif strings == "June":
-                June.append(1)
-            elif strings == "July":
-                July.append(1)
-            elif strings == "August":
-                August.append(1)
-            elif strings == "September":
-                September.append(1)
-            elif strings == "October":
-                October.append(1)
-            elif strings == "November":
-                November.append(1)
-            elif strings == "December":
-                December.append(1)
+        month_list = [[], [], [], [], [], [], [], [], [], [], [], []]
 
+        x = 0
+        while x <= 11:
+            for strings in result:
+                if strings == months[x]:
+                    month_list[x].append(1)
+            x += 1
 
-    def print_publisher(webScraping, setter):
+    def print_publisher(self):
         # Data to plot
         labels = ['January', 'February', 'March', 'April', 'May', 'June',
                   'July', 'August', 'September', 'October',
                   'November', 'December']
-        sizes = [len(January), len(February), len(March), len(April), len(May), len(June), len(July),
-                 len(August), len(September), len(October), len(November), len(December)]
+        sizes = [len(month_list[0]), len(month_list[1]), len(month_list[2]),
+                 len(month_list[3]), len(month_list[4]), len(month_list[5]),
+                 len(month_list[6]), len(month_list[7]), len(month_list[8]),
+                 len(month_list[9]), len(month_list[10]), len(month_list[11])]
         colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue',
                   'red', 'black', 'blue', 'yellow', 'green',
                   'purple', 'orange', 'darkblue']
@@ -69,61 +42,61 @@ class Data_Methods(object):
         plt.axis('equal')
         plt.show()
 
-    def price_comparison(webScraping):
-            x = []
-            count = 0
-            x1 = []
-            count1 = 0
-            intlist = [float(x) for x in webScraping.RRP()]
-            salePriceInt = [float(x) for x in webScraping.sale_prices()]
-            while count < len(webScraping.RRP()):
-                count += 1
-                x.append(count)
+    def price_comparison(webscraping):
+        x = []
+        count = 0
+        x1 = []
+        count1 = 0
+        intlist = [float(x) for x in webscraping.RRP()]
+        salePriceInt = [float(x) for x in webscraping.sale_prices()]
+        while count < len(webscraping.RRP()):
+            count += 1
+            x.append(count)
 
-            while count1 < len(webScraping.sale_prices()):
-                count1 += 1
-                x1.append(count1)
-            plt.figure(num=None, figsize=(10, 10), dpi=80,
-                       facecolor='w', edgecolor='k')
-            y = [i for i in intlist]
-            y1 = [i for i in salePriceInt]
-            plt.xlim(0, 22)
-            plt.plot(x, y, label='RRP')
-            plt.plot(x1, y1, label='Sale Price', color='r')
-            plt.scatter(x, y)
-            plt.scatter(x1, y1,  color='r')
-            plt.title('Price Comparison Chart')
-            plt.ylabel('Price ($)')
-            plt.xlabel('Number of products')
-            for i, j in zip(y1, x1):
-                plt.annotate(str(i), xy=(j, i), textcoords='data',
-                             horizontalalignment='left',
-                             verticalalignment='bottom')
-            for i, j in zip(y, x):
-                plt.annotate(str(i), xy=(j, i), textcoords='data',
-                             horizontalalignment='left',
-                             verticalalignment='bottom')
-            plt.legend()
-            plt.show()
+        while count1 < len(webscraping.sale_prices()):
+            count1 += 1
+            x1.append(count1)
+        plt.figure(num=None, figsize=(10, 10), dpi=80,
+                   facecolor='w', edgecolor='k')
+        y = [i for i in intlist]
+        y1 = [i for i in salePriceInt]
+        plt.xlim(0, 22)
+        plt.plot(x, y, label='RRP')
+        plt.plot(x1, y1, label='Sale Price', color='r')
+        plt.scatter(x, y)
+        plt.scatter(x1, y1, color='r')
+        plt.title('Price Comparison Chart')
+        plt.ylabel('Price ($)')
+        plt.xlabel('Number of products')
+        for i, j in zip(y1, x1):
+            plt.annotate(str(i), xy=(j, i), textcoords='data',
+                         horizontalalignment='left',
+                         verticalalignment='bottom')
+        for i, j in zip(y, x):
+            plt.annotate(str(i), xy=(j, i), textcoords='data',
+                         horizontalalignment='left',
+                         verticalalignment='bottom')
+        plt.legend()
+        plt.show()
 
-    def total_savings_data(webScraping):
-            count = 0
-            x = []
-            str_list = filter(len, webScraping.saving_total())
-            intlist = [int(x) for x in str_list]
-            while count < len(intlist):
-                    count += 1
-                    x.append(count)
-            plt.figure(num=None, figsize=(8, 8), dpi=80, facecolor='w',
-                       edgecolor='k')
-            y = [i for i in intlist]
-            plt.xlim(0, 22)
-            plt.ylim(0, 100)
-            plt.bar(x, y)
-            plt.title('Total Savings')
-            plt.ylabel('Savings (%)')
-            for i, j in zip(y, x):
-                plt.annotate(str(i), xy=(j, i), textcoords='data',
-                             horizontalalignment='left',
-                             verticalalignment='bottom')
-            plt.show()
+    def total_savings_data(webscraping):
+        count = 0
+        x = []
+        str_list = filter(len, webscraping.saving_total())
+        intlist = [int(x) for x in str_list]
+        while count < len(intlist):
+            count += 1
+            x.append(count)
+        plt.figure(num=None, figsize=(8, 8), dpi=80, facecolor='w',
+                   edgecolor='k')
+        y = [i for i in intlist]
+        plt.xlim(0, 22)
+        plt.ylim(0, 100)
+        plt.bar(x, y)
+        plt.title('Total Savings')
+        plt.ylabel('Savings (%)')
+        for i, j in zip(y, x):
+            plt.annotate(str(i), xy=(j, i), textcoords='data',
+                         horizontalalignment='left',
+                         verticalalignment='bottom')
+        plt.show()
